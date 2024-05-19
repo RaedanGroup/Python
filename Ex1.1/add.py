422f83c3d6a8f3ef5804d0df76f6458a9944b303
Argos Stone <peter@raedangroup.com>
2024-05-18 22:47:36 -0400

    Add files via upload

---
+++ Ex1.1/add.py
@@ -0,0 +1,4 @@
+first_number = int(input("prompt for first number: "))
+second_number = int(input("prompt for second number: "))
+sum_of_numbers = first_number + second_number
+print(sum_of_numbers)
